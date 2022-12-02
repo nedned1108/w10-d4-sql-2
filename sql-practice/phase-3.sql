4ce87257dcc2bebb4d1cfef984bd45f0c1221040
jlin231 <jonlin981@gmail.com>
2022-12-02 14:14:12 -0800

    phase 3/4

--- sql-practice/phase-3.sql
+++ sql-practice/phase-3.sql
@@ -1,5 +1,18 @@
 -- Find Hermione's cats
 -- Your code here
 
+-- SELECT cats.name, owners.first_name FROM cat_owners
+-- JOIN cats ON cats.id=cat_owners.cat_id
+-- JOIN owners ON owners.id = cat_owners.owner_id
+-- WHERE first_name="Hermione";
+
 -- Find All the Toys for Hermione's cats
 -- Your code here
+
+SELECT cats.name, toys.name, owners.first_name FROM cat_owners
+JOIN cats ON cats.id=cat_owners.cat_id
+JOIN owners ON owners.id = cat_owners.owner_id
+JOIN toys ON toys.cat_id = cats.id
+WHERE owners.first_name = "Hermione"; 
+
+
--- sql-practice/phase-4.sql
+++ sql-practice/phase-4.sql
@@ -1,5 +1,34 @@
 PRAGMA foreign_keys=on; -- Turns foreign key support in SQLite3 on
--- Your code here
+.headers on
+.mode column
+
+DROP TABLE IF EXISTS owners;
+DROP TABLE IF EXISTS cats;
+DROP TABLE IF EXISTS cat_owners;
+DROP TABLE IF EXISTS toys;
+
+CREATE TABLE owners (
+  id INTEGER PRIMARY KEY,
+  first_name TEXT,
+  last_name TEXT
+);
+
+CREATE TABLE cats (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  birth_year INTEGER
+);
+
+CREATE TABLE cat_owners (
+  cat_id INTEGER REFERENCES cats(id) ON DELETE CASCADE, 
+  owner_id INTEGER REFERENCES owners(id) ON DELETE CASCADE
+);
+
+CREATE TABLE toys (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  cat_id INTEGER REFERENCES cats(id) ON DELETE CASCADE
+);
 
 INSERT INTO owners
   (first_name, last_name)
